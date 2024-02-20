e24d7140b5c08a3b02679fc6853cba910034721b
yawoong2 <adamzoong@naver.com>
2024-02-21 00:12:57 +0900

    refactor: Node class의 제너릭 타입 명을 T로 변경

--- Calculator/Calculator/Model/Node.swift
+++ Calculator/Calculator/Model/Node.swift
@@ -5,11 +5,11 @@
 //  Created by JIWOONG on 2024/02/17.
 //
 
-class Node<E> {
-    let data: E
+class Node<T> {
+    let data: T
     var next: Node?
     
-    init(data: E, next: Node?) {
+    init(data: T, next: Node?) {
         self.data = data
         self.next = next
     }
